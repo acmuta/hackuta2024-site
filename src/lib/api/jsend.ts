bff5ddcdfe4c37e7374c4ea826d56c16aa9c4e79
nhitran26197 <nhitran26197@gmail.com>
2024-05-15 17:54:39 -0500

    working on sending email

---
+++ src/lib/api/jsend.ts
@@ -0,0 +1,36 @@
+// JSend is a specification for JSON-based application-level communication.
+// https://github.com/omniti-labs/jsend
+
+import type { NextApiResponse } from 'next'
+
+import { stringifyError } from '@/lib/utils/shared'
+
+const jsend = Object.freeze({
+    error: <E = unknown>(message: unknown, data?: E): JSendError<E> => ({
+        status: 'error',
+        data,
+        message: stringifyError(message),
+    }),
+    success: <T>(data: T): JSendSuccess<T> => ({
+        status: 'success',
+        data,
+    }),
+})
+
+export default jsend
+
+export type NextJSendResponse<T, E = unknown> = NextApiResponse<JSend<T, E>>
+
+export type JSend<T, E = unknown> = JSendSuccess<T> | JSendError<E>
+
+export interface JSendError<E = unknown> {
+    status: 'error'
+    code?: number
+    data?: E
+    message: string
+}
+
+export interface JSendSuccess<T> {
+    status: 'success'
+    data: T
+}
---
+++ src/lib/auth/server.ts
@@ -0,0 +1,31 @@
+import '@/node-only'
+
+import clientPromise from '../db'
+import { AppPermissions, Role } from '../db/models/Role'
+import User from '../db/models/User'
+import { BuiltInRoles, mergePermission } from './shared'
+
+export * from './shared'
+
+export async function getUserPerms(user: User | null): Promise<AppPermissions> {
+    const roles = [
+        ...(user ? ['hacker'] : []),
+        ...(user?.roles ?? []),
+    ]
+    const builtInRoles = roles.filter((r): r is keyof typeof BuiltInRoles =>
+        r in BuiltInRoles
+    )
+    const externalRoles = roles.filter((r) => !(r in BuiltInRoles))
+    const client = await clientPromise
+    const perms = [
+        ...builtInRoles.map((r) => BuiltInRoles[r]),
+        ...(await client.db()
+            .collection<Role>('roles')
+            .find({
+                _id: { $in: externalRoles },
+            })
+            .toArray())
+            .map((v) => v.granted),
+    ]
+    return mergePermission(BuiltInRoles['@@base'], ...perms)
+}
