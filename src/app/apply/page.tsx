10223dcbd36b2bf973f8411fe6d25482c09b6708
dinklang <sdinklang@gmail.com>
2024-10-12 10:51:07 -0500

    Opened applications

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -2,7 +2,7 @@ import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 import { getEnhancedSession } from '@/lib/utils/server'
-// import { ApplicationForm } from './ApplicationForm'
+import { ApplicationForm } from './ApplicationForm'
 
 export default function Home() {
     const { user } = getEnhancedSession(headers())
@@ -11,11 +11,12 @@ export default function Home() {
     } else if (user.applied) {
         redirect('/dashboard')
     } else {
-        return (
-            <p style={{ color: 'white', textAlign: 'center' }}>
-                Applications are now closed. Thanks for you interest in HackUTA
-                6!
-            </p>
-        )
+        return <ApplicationForm />
+        // return (
+        //     <p style={{ color: 'white', textAlign: 'center' }}>
+        //         Applications are now closed. Thanks for you interest in HackUTA
+        //         6!
+        //     </p>
+        // )
     }
 }
